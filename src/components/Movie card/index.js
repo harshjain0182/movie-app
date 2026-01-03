1d5687b935c5103603cdbafec9ce0a2f25a1381b
harshjain0182 <harsh.23mcan0182@jecrcu.edu.in>
2026-01-03 13:34:46 +0530

    dispalying movies according to search

---
+++ src/components/Movie card/index.js
@@ -0,0 +1,54 @@
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Grid, IconButton } from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+export default function MovieCard({ movie }) {
+    return (
+        <Grid item xs={12}>
+            <Card sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column"
+            }}>
+                <CardMedia
+                    sx={{ height: 200 }}
+                    image={movie.img_link}
+                    title={movie.name}
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {movie.name}
+                    </Typography>
+                    <Typography gutterBottom variant="h7" component="div">
+                        Director By: {movie.director_name}
+                    </Typography>
+                    <Typography gutterBottom variant="h7" component="div">
+                        Witten By: {movie.writer_name}
+                    </Typography>
+                    <Typography gutterBottom variant="h7" component="div">
+                        Genre: {movie.genres}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <Typography gutterBottom variant="h7" component="div">
+                        {movie.duration_in_mins} mins
+                    </Typography>
+                    <Typography gutterBottom variant="h7" component="div">
+                        {movie.imdb_rating}⭐
+                    </Typography>
+                    <Typography gutterBottom variant="h7" component="div">
+                        {movie.imdb_votings}
+                    </Typography>
+                </CardActions>
+            </Card>
+        </Grid>
+    );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,34 @@
 import { useEffect } from "react";
-import { getMovies} from "../../api/movies";
+import { getMovies } from "../../api/movies";
 import Navbar from "../../components/Navbar"
 import { useDispatch, useSelector } from "react-redux";
-
+import MovieCard from "../../components/Movie card";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { getSearchedMovies } from "../../utils/searchedMovies";
 const Home = () => {
 
     const dispatch = useDispatch();
 
-    const {movies} = useSelector(state => state.movies)
-    console.log(movies);
-
-    useEffect( () => {
+    const { movies, searchState } = useSelector(state => state.movies)
+    const filterdMovies = getSearchedMovies(movies, searchState);
+    
+    useEffect(() => {
         dispatch(getMovies());
     }, []);
-    
-    return(
-        <Navbar/>
+
+    return (
+        <>
+            <Navbar />
+            <Box sx={{ flexGrow: 1, marginTop: 2 }}>
+                <Grid container spacing={2}>
+                    {
+                        filterdMovies.length > 0 && filterdMovies.map(movie => <MovieCard key={movie.id} movie={movie} />)
+                    }
+
+                </Grid>
+            </Box>
+        </>
     )
 }
 
