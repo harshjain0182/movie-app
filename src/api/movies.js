5e0c5411d52a1db05101dff3a54a2f7feb114247
harshjain0182 <harsh.23mcan0182@jecrcu.edu.in>
2026-01-04 15:59:34 +0530

    Genre Filtering done

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,30 +1,58 @@
 import axios from 'axios';
-import { setMovies, setSearchValue } from '../slice/movieSlice';
+import { setMovies, setGenres } from '../slice/movieSlice';
+import { UNSAFE_DataRouterStateContext } from 'react-router-dom';
 
 const BaseUrl = "https://83b05cfb-e74c-49f8-8ed4-09abdf015696-00-2mn4a00imlo4f.pike.replit.dev:3000/api/movies";
 
 export const getMovies = () => async dispatch => {
 
-    try{
-        const {data}= await axios.get(BaseUrl);
+    try {
+        const { data } = await axios.get(BaseUrl);
         dispatch(setMovies(data));
     }
-    catch(err){
-        console.log("Not able to fetch data", err);
+    catch (err) {
+        return err;
     }
 }
 
-export const getMoviesBySearch = (value) => async dispatch =>{
+export const getMoviesBySearch = (value) => async dispatch => {
     const url = BaseUrl + "/search"
 
-    try{
-        const {data} = await axios.get(url,{
-            params : {
-                search : value
+    try {
+        const { data } = await axios.get(url, {
+            params: {
+                search: value
             }
         });
         dispatch(setMovies(data));
-    } catch(err){
+    } catch (err) {
         return err;
     }
+}
+
+export const getALlGenres = () => async dispatch => {
+    const url = BaseUrl + '/genres'
+    try {
+        const { data } = await axios.get(url);
+        dispatch(setGenres(data));
+    }
+    catch (err) {
+        return err;
+    }
+}
+
+export const getMovieByGenre = (value) => async dispatch => {
+    const url = BaseUrl + '/genre';
+
+    try {
+        const { data } = await axios.get(url, {
+            params: {
+                genre: value
+            }
+        });
+        dispatch(setMovies(data));
+    } catch (err) {
+        return err;
+    }
+
 }
--- src/components/Selector/index.js
+++ src/components/Selector/index.js
@@ -1,31 +1,40 @@
-import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { useDispatch } from 'react-redux';
+import { getMovieByGenre } from '../../api/movies';
 
-export default function SelectorComponent() {
-  const [age, setAge] = React.useState('');
+export default function SelectorComponent({name, value}) {
 
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const dispatch = useDispatch();
+  
   const handleChange = (event) => {
-    setAge(event.target.value);
+    setSelectedValue(event.target.value);
+    dispatch(getMovieByGenre(event.target.value));
   };
+  console.log(selectedValue);
+  
 
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <InputLabel id="demo-simple-select-label">{name}</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={age}
-          label="Age"
+          value={selectedValue}
+          label={name}
           onChange={handleChange}
         >
-          <MenuItem value={10}>Ten</MenuItem>
-          <MenuItem value={20}>Twenty</MenuItem>
-          <MenuItem value={30}>Thirty</MenuItem>
+          {
+            value.length > 0 && value.map(item => <MenuItem  value={name.toLowerCase().includes('genres') ? item.genre :item.ratings}>{name.toLowerCase().includes('genres') ? item.genre :item.ratings}</MenuItem>)
+          }
+
         </Select>
       </FormControl>
     </Box>
